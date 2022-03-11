415ff0c2986ff64e98282345dcee83b297e01be4
Rafael Alves <rafael.alves@live.com>
2022-03-11 19:47:01 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Customer.io Plugin
+
+Send event data into [Customer.io](https://customer.io/). User emails will also be sent if available.
+
+![Customer.io Screenshot](readme-assets/customer-io.png)
+
+## Installation
+
+1. Visit 'Project Plugins' under 'Settings'
+1. Enable plugins if you haven't already done so
+1. Click the 'Repository' tab next to 'Installed'
+1. Click 'Install' on this plugin
+1. Add your [Customer.io](https://customer.io/) site ID and token at the configuration step
+1. Enable the plugin and watch your 'People' list get populated in Customer.io!
---
+++ index.js
@@ -0,0 +1,151 @@
+import { createBuffer } from '@posthog/plugin-contrib'
+
+export async function setupPlugin({ config, global }) {
+    
+    const customerioBase64AuthToken = Buffer.from(`${config.customerioSiteId}:${config.customerioToken}`).toString(
+        'base64'
+    )
+
+    global.customerioAuthHeader = {
+        headers: {
+            Authorization: `Basic ${customerioBase64AuthToken}`
+        }
+    }
+
+    const authResponse = await fetchWithRetry(
+        'https://beta-api.customer.io/v1/api/info/ip_addresses',
+        global.customerioAuthHeader
+    )
+    
+    if (statusUnauthorized(authResponse)){
+        throw new Error('Unable to connect to Customer.io')
+    }
+
+    if (!statusOk(authResponse)) {
+        throw new RetryError('Service is down, retry later')
+    }
+
+    const eventNames = config.eventsToSend ? config.eventsToSend.split(',').filter(Boolean) : []
+
+    global.buffer = createBuffer({
+        limit: (1 / 5) * 1024 * 1024, // 200kb
+        timeoutSeconds: 5,
+        onFlush: async (batch) => {
+            console.log(`Flushing batch of length ${batch.length}`)
+            for (const event of batch) {
+                if (eventNames.length > 0 && !eventNames.includes(event.event)) { 
+                    continue
+                }
+
+                if (event.event === 'APP - Device Link') {
+                    exportPersonDeviceToCustomerio(event, global.customerioAuthHeader)
+                    continue
+                }
+
+                if (event.event === '$identify') {
+                    await exportPersonToCustomerio(event, global.customerioAuthHeader)
+                    continue
+                }
+                
+                await exportToCustomerio(event, global.customerioAuthHeader)
+            }
+        }
+    })
+}
+
+export async function onEvent(event, { global }) {
+    global.buffer.add(event)
+}
+
+async function exportToCustomerio(event, authHeader) {
+    const eventInsertResponse = await fetchWithRetry(
+        `https://track.customer.io/api/v1/customers/${event.distinct_id}/events`,
+        {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                ...authHeader.headers
+            },
+            body: JSON.stringify({ name: event.event, data: event.properties })
+        },
+        'POST'
+    )
+
+    if (!statusOk(eventInsertResponse)) {
+        console.log(`Unable to send event ${event.event} to Customer.io`)
+    }
+}
+
+async function exportPersonToCustomerio(event, authHeader) {
+    console.log(`Identify person ${event.distinct_id}`, event)
+
+    const personInsertResponse = await fetchWithRetry(
+        `https://track.customer.io/api/v1/customers/${event.distinct_id}`,
+        {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                ...authHeader.headers
+            },
+            body: JSON.stringify({ email: event.$set.$email, name: event.$set.$name, ...event.$set })
+        },
+        'PUT'
+    )
+
+    if (!statusOk(personInsertResponse)) {
+        console.log(`Unable to send event ${event.event} to Customer.io`)
+    }
+}
+
+async function exportPersonDeviceToCustomerio(event, authHeader) {
+    console.log(`Send person (${event.distinct_id}) device`, event)
+
+    try {
+        const deviceUpdateResponse = await fetchWithRetry(
+            `https://track.customer.io/api/v1/customers/${event.distinct_id}/devices`,
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    ...authHeader.headers
+                },
+                body: JSON.stringify({
+                    device: {
+                        id: event.properties?.deviceToken,
+                        platform: event.properties?.$os_name?.toLowerCase()
+                    }
+                 })
+            },
+            'PUT'
+        )
+    
+        if (!statusOk(deviceUpdateResponse)) {
+            console.log(`Unable to send person (${event.distinct_id}) device to Customer.io: ${JSON.stringify(deviceUpdateResponse)}`)
+        }
+    } catch (error) {
+        console.log(`Error sending person (${event.distinct_id}) device to Customer.io`)
+    }
+}
+
+async function fetchWithRetry(url, options = {}, method = 'GET', isRetry = false) {
+    try {
+        const res = await fetch(url, { method: method, ...options })
+        return res
+    } catch {
+        if (isRetry) {
+            throw new Error(`${method} request to ${url} failed.`)
+        }
+        const res = await fetchWithRetry(url, options, (method = method), (isRetry = true))
+        return res
+    }
+}
+
+function statusOk(res) {
+    return Math.floor(res.status / 100) === 2
+}
+
+function statusUnauthorized(res){
+    return res.status == 401
+}
+
+function isEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(String(email).toLowerCase())
+}
